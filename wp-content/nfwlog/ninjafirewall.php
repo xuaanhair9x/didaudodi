81f3cf3fb9445de27d5503a0a65c1735edf6de43
xuanhai <hai.nguyen@fidesio.com>
2020-08-24 19:05:55 +0700

    update git ignore

--- wp-content/nfwlog/ninjafirewall.php
+++
@@ -1,9 +0,0 @@
-<?php
-// ===============================================================//
-// NinjaFirewall's loader.                                        //
-// DO NOT alter or remove it as long as NinjaFirewall is running! //
-// ===============================================================//
-if ( file_exists('/Users/xuanhai/www/didaudodi/wp-content/plugins/ninjafirewall/lib/firewall.php') ) {
-	@include_once '/Users/xuanhai/www/didaudodi/wp-content/plugins/ninjafirewall/lib/firewall.php';
-}
-// EOF
--- wp-content/nfwlog/readme.txt
+++
@@ -1,3 +0,0 @@
-This is NinjaFirewall's logs, loader and cache directory. DO NOT alter or remove it as long as NinjaFirewall is running!
-
-If you just uninstalled NinjaFirewall, WAIT 5 MINUTES before deleting this folder, otherwise your site will likely crash.
